e20759e89c20ea5504f505728b6e3fada40dea8a
silvano36 <silvanussigei1996@gmail.com>
2024-02-24 19:31:05 +0300

    initial repo

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-console.log('hello world')
+const express =require('express')
+const dotenv = require('dotenv').config()
+const port = 5000
+
+const app = express()
+
+app.listen(port,()=> console.log(`server started on port ${port}`))
