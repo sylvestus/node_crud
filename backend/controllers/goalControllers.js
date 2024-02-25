138ec393bf6f13b18e1e37efa5f1c35f5c704322
silvano36 <silvanussigei1996@gmail.com>
2024-02-25 03:18:57 +0300

    mongo db connection and manipulation

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -1,11 +1,12 @@
 const asyncHandler = require('express-async-handler')
+const Goal = require('../models/goalsModel')
 // @desc gets goals
 // @route GET /api/goals
 // @access Private
 // when interacting with the database you get a promise hence use asynv await
 const getGoals = asyncHandler(async (req, res) => {
-
-  res.status(200).json({ message: "Get goals" });
+  const goals = await Goal.find()
+  res.status(200).json(goals);
 });
 // @desc posts goals
 // @route POST /api/goals
@@ -15,19 +16,34 @@ const setGoals = asyncHandler(async  (req, res) => {
     res.status(400)
     throw new Error('please add text field')
   }
-  res.status(200).json({ message: "Set goal" })
+  const goal = await Goal.create({
+    text:req.body.text
+  })
+  res.status(200).json(goal);
 });
 // @desc updates goals
 // @route PUT /api/goals/id
 // @access Private 
 const updateGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update goal ${req.params.id}` });
+    const goal = await Goal.findById(req.params.id)
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id,req.body,{new:true})
+  res.status(200).json(updatedGoal);
 });
 // @desc deletes goals
 // @route DELETE /api/goals/id
 // @access Private 
 const deleteGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `delete goal ${req.params.id}` });
+     const goal = await Goal.findById(req.params.id);
+     if (!goal) {
+       res.status(400);
+       throw new Error("Goal not found");
+     }
+     await Goal.deleteOne({ _id: req.params.id});
+  res.status(200).json({ message: `deleted goal ${req.params.id}` });
 });
 
 module.exports = {
